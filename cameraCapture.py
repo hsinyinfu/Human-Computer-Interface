4b5a27e6263edf3e6f7e7f0dbd32fef646611888
hsinyinfu <peter0930260535@gmail.com>
2017-11-11 00:52:36 +0800

    Test for smoothing by GaussianBlur.

--- cameraCapture.py
+++ cameraCapture.py
@@ -2,6 +2,9 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
+
+# Test for using camera by openCV
+'''
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -12,6 +15,19 @@ while(True):
     # Display the resulting frame
     cv2.imshow('frame',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+'''
+
+# Test for smoothing by GaussianBlur
+while(True):
+    t0 = cap.read()[1]
+    grey1 =  cv2.cvtColor(t0, cv2.COLOR_BGR2GRAY)
+    blur1 = cv2.GaussianBlur(grey1,(7,7),0)
+    cv2.imshow('frame', t0)
+    cv2.imshow('grey', grey1)
+    cv2.imshow('blur', blur1)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
         break
 
 # When everything done, release the capture
