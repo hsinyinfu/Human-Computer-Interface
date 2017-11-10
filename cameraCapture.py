5caf34ed5d69d342919adc2d8c75ee8c04a4d9e3
hsinyinfu <peter0930260535@gmail.com>
2017-11-11 00:55:35 +0800

    Test for "Background Subtraction".

--- cameraCapture.py
+++ cameraCapture.py
@@ -19,6 +19,7 @@ while(True):
 '''
 
 # Test for smoothing by GaussianBlur
+'''
 while(True):
     t0 = cap.read()[1]
     grey1 =  cv2.cvtColor(t0, cv2.COLOR_BGR2GRAY)
@@ -26,6 +27,21 @@ while(True):
     cv2.imshow('frame', t0)
     cv2.imshow('grey', grey1)
     cv2.imshow('blur', blur1)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+'''
+
+# Test for background subtraction.
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
+while(1):
+    ret, frame = cap.read()
+
+    fgmask = fgbg.apply(frame)
+    #print(fgmask)
+
+    cv2.imshow('frame',fgmask)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
