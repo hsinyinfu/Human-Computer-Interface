de0306499e6f22d52185d457ec2ed5d9e435b3b9
hsinyinfu <peter0930260535@gmail.com>
2017-11-11 11:31:56 +0800

    完成指令一：拍照並存檔，以及拍照前會有倒數的字幕

--- backgroundSubsctraction.py
+++ backgroundSubsctraction.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import time
 
 MOVEMENT_THRESHOLD = 50
 ACCUMULATE_THRESHOLD = 5
@@ -7,7 +8,33 @@ ACCUMULATE_THRESHOLD = 5
 cap = cv2.VideoCapture(0)
 fgbg = cv2.createBackgroundSubtractorMOG2()
 count = 0
+startTime = None
+photoCount = 0
+font = cv2.FONT_HERSHEY_SIMPLEX
 
+def takePicture():
+    global startTime, frame, font, photoCount
+    while True:
+        waited = time.time() - startTime
+        if waited >= 4:
+            break
+        elif waited >= 3:
+            #print(3)
+            cv2.putText(frame,'1',(10,500), font,5,(255,255,255),2,cv2.LINE_AA)
+        elif waited >= 2:
+            #print(2)
+            cv2.putText(frame,'2',(10,500), font,5,(255,255,255),2,cv2.LINE_AA)
+        else:
+            #print(1)
+            cv2.putText(frame,'3',(10,500), font,5,(255,255,255),2,cv2.LINE_AA)
+        cv2.imshow('frame',frame)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+        frame = cap.read()[1]
+    photo = cap.read()[1]
+    photoCount += 1
+    cv2.imwrite('photo-'+str(photoCount)+'.jpg',photo)
 
 # main function
 while(1):
@@ -19,11 +46,12 @@ while(1):
     #print(np.average(fgmask))
     if(np.average(fgmask) >= MOVEMENT_THRESHOLD):
         count += 1
-        print(count)
+        #print(count)
         if(count >= ACCUMULATE_THRESHOLD):
-            print("succeed")
+            #print("succeed")
             count = 0
-            # do something
+            startTime = time.time()
+            takePicture()
             frame = cap.read()[1]
 
     cv2.imshow('frame',frame)
@@ -33,3 +61,4 @@ while(1):
 
 cap.release()
 cv2.destroyAllWindows()
+
